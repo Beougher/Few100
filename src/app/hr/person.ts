369b07c4fd2b505c7e81f2645b1a54174b867e55
Beougher <96155211+Beougher@users.noreply.github.com>
2021-12-15 12:31:03 -0500

    Changes

---
+++ src/app/hr/person.ts
@@ -0,0 +1,8 @@
+export abstract class Person {
+  firstName = '';
+  lastName = '';
+  get fullName() {
+    return `${this.lastName}, ${this.firstName}`;
+  }
+
+}
--- src/app/utils/math.ts
+++ src/app/utils/math.ts
@@ -3,6 +3,10 @@ export function isEven(n: number): boolean {
   return n % 2 === 0;
 }
 
+export function isEvenTruncateDecimal(n: number): boolean {
+  const val = Math.floor(n);
+  return isEven(val);
+}
 
 export const PI = 3.1415927;
 
